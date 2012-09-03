042fdb001ebe5eb94061c09d37b5d3b290a63d1e
Michael Edoror <medoror@gmail.com>
2012-09-03 12:31:35 -0400

    starting to build new app widget after figuring how it should be done

--- gen/bank/machine/app/R.java
+++ gen/bank/machine/app/R.java
@@ -11,14 +11,17 @@ public final class R {
     public static final class attr {
     }
     public static final class drawable {
-        public static final int background=0x7f020000;
-        public static final int ic_launcher=0x7f020001;
-        public static final int launcher=0x7f020002;
-        public static final int minus=0x7f020003;
-        public static final int plus=0x7f020004;
+        public static final int add_button=0x7f020000;
+        public static final int background=0x7f020001;
+        public static final int ic_launcher=0x7f020002;
+        public static final int launcher=0x7f020003;
+        public static final int minus=0x7f020004;
+        public static final int new_title=0x7f020005;
+        public static final int plus=0x7f020006;
+        public static final int sub_button=0x7f020007;
     }
     public static final class id {
-        public static final int Corners=0x7f060010;
+        public static final int Corners=0x7f060011;
         public static final int account_name_entry=0x7f060005;
         public static final int button1=0x7f060001;
         public static final int button2=0x7f060004;
@@ -26,15 +29,16 @@ public final class R {
         public static final int button4=0x7f060008;
         public static final int editText2=0x7f060003;
         public static final int editText3=0x7f060000;
-        public static final int editText4=0x7f06000b;
-        public static final int editText5=0x7f06000e;
-        public static final int editText6=0x7f06000f;
+        public static final int editText5=0x7f06000f;
+        public static final int editText6=0x7f060010;
         public static final int imageButton1=0x7f060009;
-        public static final int imageButton2=0x7f06000c;
+        public static final int imageButton2=0x7f06000a;
+        public static final int imageView1=0x7f06000b;
         public static final int number_entry=0x7f060006;
         public static final int spinner1=0x7f060002;
-        public static final int spinner2=0x7f06000d;
-        public static final int spinner3=0x7f06000a;
+        public static final int spinner2=0x7f06000e;
+        public static final int textView1=0x7f06000c;
+        public static final int textView2=0x7f06000d;
     }
     public static final class layout {
         public static final int create_account=0x7f030000;
